f5a3b07070c17d7a6b4f4f348c0663773767e4a8
Shawn Melton <shawn.a.melton@gmail.com>
2014-03-25 07:30:17 -0400

    Initial site build

---
+++ Gruntfile.js
@@ -0,0 +1,96 @@
+module.exports = (function(grunt) {
+    'use strict';
+
+    grunt.initConfig({
+        /*jst: {
+            compile: {
+                files: {
+                    'src/js/templates/jst.js': 'src/js/templates/*.html'
+                }
+            }
+        },
+        jshint: {
+            all: ['Gruntfile.js', 'src/js/*.js', 'src/js/views/*.js']
+        },
+        requirejs: {
+            compile: {
+                options: {
+                    optimize: 'uglify',
+                    preserveLicenseComments: false,
+                    findNestedDependencies: true,
+                    baseUrl: 'src/js/',
+                    mainConfigFile: 'src/js/bootstrap.js',
+                    name: 'app',
+                    include: ['bootstrap'],
+                    out: 'tmp/js/app.js'
+                }
+            }
+        },
+        uglify: {
+            options: {
+                mangle: true,
+                compress: true,
+                report: 'min'
+            },
+            my_target: {
+                files: {
+                    'dist/js/app.js': ['src/js/libs/require.js', 'tmp/js/app.js']
+                }
+            }
+        },*/
+        sass: {
+            dist: {
+                files: {
+                    'dist/css/styles.css': 'src/css/styles.scss'
+                },
+                options: {
+                    style: 'compressed'
+                }
+            }
+        },
+        imagemin: {
+            options: {
+                optimizationLevel: 3
+            },
+            sitesImages: {
+                files: [{
+                    expand: true,
+                    cwd: 'src/img/',
+                    src: ['*.png', '*.jpg', '*.gif', '*/*.jpg'],
+                    dest: 'dist/img/'
+                }]
+            }
+        },
+        htmlmin: {
+            dist: {
+                options: {
+                    removeComments: true,
+                    collapseWhitespace: true
+                },
+                files: { 
+                    'dist/index.html': 'src/index.html'
+                }
+            }
+        },
+        clean: ['tmp', 'dist/img', 'dist/js', 'dist/css', 'dist/index.html']
+    });
+
+    grunt.event.on('watch', function(action, filepath) {
+        grunt.log.writeln(filepath +' has '+ action);
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-htmlmin');
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    /*grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-jst');
+    grunt.loadNpmTasks('grunt-contrib-requirejs');
+    grunt.loadNpmTasks('grunt-contrib-uglify');*/
+
+    grunt.registerTask('default', [
+        /*'jst', 'jshint', 'requirejs', 'uglify',*/ 'sass', 'htmlmin', 'imagemin'
+    ]);
+
+    grunt.registerTask('cleanup', ['clean']);
+});
