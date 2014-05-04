1a20d66e6016cc283d55912cfbedf21b7faad2ac
Shawn Melton <shawn.a.melton@gmail.com>
2014-05-04 07:43:10 -0400

    More home page styling.

--- src/js/templates/jst.js
+++ src/js/templates/jst.js
@@ -4,7 +4,7 @@ this["JST"]["src/js/templates/home.html"] = function(obj) {
 obj || (obj = {});
 var __t, __p = '', __e = _.escape;
 with (obj) {
-__p += '<section id="spotlight">\n    <div></div>\n    <h1><span>Experience Renaissance Movement Music</span></h1>\n    <p id="call-to-actions">\n        <a id="home-events">Upcoming Events</a>\n        <a id="home-book">Book Now</a>\n    </p>\n    <p>\n        <a id="home-play"></a>\n        <a id="home-open">Open <span>RMTV</span></a>\n    </p>\n</section>';
+__p += '<section id="spotlight">\n    <div></div>\n    <h1><span>Experience Renaissance Movement Music</span></h1>\n    <p id="calls-to-action">\n        <a id="home-events">\n            <span></span>\n            <strong>Upcoming Events</strong>\n        </a>\n        <a id="home-book">\n            <span></span>\n            <strong>Book Now</strong>\n        </a>\n    </p>\n    <a id="home-play"></a>\n    <p id="open-rmtv"><a>Open <span>RMTV</span></a></p>\n</section>';
 
 }
 return __p
